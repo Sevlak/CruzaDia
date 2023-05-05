8a2b96f45f24857d3c3cb1c109299b5a5a56cd28
William Alves <willoandrade29@gmail.com>
2023-05-04 23:36:09 -0300

    feat: add index page and view imports and starting layouts
    This commit adds the Index page and two files: _ViewImports.cshtml (applies imports to all subfolders) and _ViewStart.cshtml (applies content and layout to all subfolders)

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CruzaDia.Pages
+{
+    public class Index : PageModel
+    {
+        public string Message = "";
+        
+        public void OnGet()
+        {
+            Message = "Hi";
+        }
+    }
+}
+
